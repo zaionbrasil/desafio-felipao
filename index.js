bdd15fbb839d345bc7c755894d9a49f53ec15451
Zaion Brasil <zaionfelix.sn@gmail.com>
2023-10-24 21:04:09 -0300

    projeto entregue

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-console.log("Oi")
+let nome = "Narutito"
+let experienciaXP = 15000
 
+
+
+if(experienciaXP < 1000 ){
+    console.log("O héroi de nome " + nome + " está no nível de Ferro" )
+}else if((experienciaXP >= 1001) && (experienciaXP <= 2000)){
+    console.log("O héroi de nome " + nome + " está no nível de Bronze")
+}else if((experienciaXP >= 2001) && (experienciaXP <= 5000)){
+    console.log("O héroi de nome " + nome + " está no nível de Prata")
+}else if((experienciaXP >=6001) && (experienciaXP<=7000)){
+    console.log("O héroi de nome " + nome + " está no nível de Ouro")
+}else if((experienciaXP >=7001) && (experienciaXP<=8000)){
+    console.log("O héroi de nome " + nome + " está no nível de Platina")
+}else if((experienciaXP >=8001) && (experienciaXP <=9000) ){
+    console.log("O héroi de nome " + nome + " está no nível de Ascendente")
+}else if((experienciaXP >=9001) && (experienciaXP <=10000)){
+    console.log("O héroi de nome " + nome + " está no nível de Imortal")
+}else if(experienciaXP >=10001){
+    console.log("O héroi de nome " + nome + " está no nível de Radiante")
+}
 
